eb202eb787037d5ce754b3367a98faf3a8aed9a3
ruthnadiezhda <a20150875@pucp.pe>
2020-05-11 18:59:52 -0500

    Primer commit

---
+++ src/main/java/com/example/laboratorio4/entity/Employees.java
@@ -0,0 +1,143 @@
+package com.example.laboratorio4.entity;
+
+import javax.persistence.*;
+import java.util.Date;
+
+
+@Entity
+@Table(name="employees")
+public class Employees {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    Integer employee_id;
+    String first_name;
+    @Column(nullable = false)
+    String last_name;
+    @Column(nullable = false)
+    String email;
+    private String password;
+    String phone_number;
+    @Column(nullable = false)
+    Date hire_date;
+    @ManyToOne
+    @JoinColumn(nullable = false, name="job_id")
+    Jobs job;
+    Double salary;
+    Double commission_pct;
+    Integer manager_id;
+    @ManyToOne
+    @JoinColumn(name="department_id")
+    Departments department;
+    private Integer enabled;
+
+    public Jobs getJob() {
+        return job;
+    }
+
+    public void setJob(Jobs job) {
+        this.job = job;
+    }
+
+    public Integer getEmployee_id() {
+        return employee_id;
+    }
+
+    public void setEmployee_id(Integer employee_id) {
+        this.employee_id = employee_id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public Date getHire_date() {
+        return hire_date;
+    }
+
+    public void setHire_date(Date hire_date) {
+        this.hire_date = hire_date;
+    }
+
+
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Double getCommission_pct() {
+        return commission_pct;
+    }
+
+    public void setCommission_pct(Double commission_pct) {
+        this.commission_pct = commission_pct;
+    }
+
+    public Integer getManager_id() {
+        return manager_id;
+    }
+
+    public void setManager_id(Integer manager_id) {
+        this.manager_id = manager_id;
+    }
+
+
+    public Departments getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Departments department) {
+        this.department = department;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Integer enabled) {
+        this.enabled = enabled;
+    }
+
+
+
+}
