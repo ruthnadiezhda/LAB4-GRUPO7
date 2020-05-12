37a0d540deb11c0431a18c15c0aee3fbb9db67a0
ruthnadiezhda <a20150875@pucp.pe>
2020-05-11 20:29:27 -0500

    ;

--- src/main/java/com/example/laboratorio4/controller/HistoryController.java
+++ src/main/java/com/example/laboratorio4/controller/HistoryController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 public class HistoryController {
 
     @Autowired
-    HistoryRepository historyRepository
+    HistoryRepository historyRepository;
 
     @GetMapping(value = {"","/"})
     public String historialEmpleado(@RequestParam("searchField") String searchField,
